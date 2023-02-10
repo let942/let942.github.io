1e656e8f55c81e89d0f48270f6d78d25f7ef8cc2
soficia <let942@gmail.com>
2023-02-10 13:43:37 -0300

    style: creates structure

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,8 @@
-import './Profile.css';
+import './Projects.css';
 
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
@@ -13,17 +14,15 @@ import profilePicture from '../assets/jpeg/me.jpg';
 function Projects() {
   return (
     <>
-      <Container fluid className='container-profile'>
+      <Container fluid className='container-projects'>
       <h2 className='mt-2 mb-1'>Projetos</h2>
-      <div className='mt-1'>
-        <ul>
-        <li>Cauculator</li>
-        <li>TrybeSmith</li>
-        <li>OneForAll</li>
-        <li>AllForOne</li>
-        <li>Algorithms</li>
-        <li>MovieManager</li>
-        </ul>
+      <div className='mt-3 mb-1 d-flex  ' >
+      
+      <div className='mt-3 mb-1 d-flex  ' > 
+      
+      
+      
+      </div>
           
         </div>
   
